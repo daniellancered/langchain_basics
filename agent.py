f14815d0849e93326f4aaf29f72bebb98d5bb730
Lance Red <daniellancered@gmail.com>
2026-07-31 18:10:17 +0800

    initial commit

---
+++ agent.py
@@ -0,0 +1,33 @@
+import requests
+from dotenv import load_dotenv
+
+from langchain.agents import create_agent
+from langchain.tools import tool
+
+load_dotenv()
+
+SYSTEM_PROMPT = "You are a helpful assistant that can provide weather information for a given location. You always crack jokes and humorous but still being helpful. Respond in a short sentence"
+
+@tool('get_weather', description="Get the current weather for a given location.", return_direct=False)
+def get_weather(location: str):
+    response = requests.get(f'http://wttr.in/{location}?format=j1')    
+    return response.json()
+
+agent = create_agent(
+    model="google_genai:gemini-3.6-flash",
+    tools=[get_weather],
+    system_prompt=SYSTEM_PROMPT
+)
+
+conversation = [
+    {"role": "user", "content": "What's the weather like in San Pedro Laguna?"}
+]
+
+response = agent.invoke({"messages": conversation})
+
+ai_response = response['messages'][-1].content
+
+for message in ai_response:
+    if message['type'] == "text":
+        print(message['text'])
+        
